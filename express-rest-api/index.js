42f97fa36506c1db6d0e981d1be10471b0964e69
Oliver Hoffman <ojh22@cornell.edu>
2023-10-05 19:20:36 +0200

    Add rest api example in nodeJS

---
+++ express-rest-api/index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3009;
+
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
+
+const users = []; // An in-memory store for our users
+
+// GET endpoint to retrieve all users
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    users.push(newUser);
+    res.status(201).json(newUser);
+});
+
+// // GET 1 user by id
+app.get('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id, 10);
+    const index = users.findIndex(user => user.id === userId);
+    
+    if (index !== -1) {
+      res.json(users[index]);
+    } else {
+      res.status(404).send('User not found');
+    }
+});
+
+// // POST endpoint to create a new user
+
+
+// // PUT endpoint to update a specific user by ID
+app.put('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id, 10);
+    const updatedUser = req.body;
+    
+    const index = users.findIndex(user => user.id === userId);
+    
+    if (index !== -1) {
+        users[index] = updatedUser;
+        res.json(updatedUser);
+    } else {
+        res.status(404).send('User not found');
+    }
+});
+
+// // DELETE endpoint to remove a user by ID
+app.delete('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id, 10);
+    const index = users.findIndex(user => user.id === userId);
+    
+    if (index !== -1) {
+        users.splice(index, 1);
+        res.status(204).send(); // No Content response
+    } else {
+        res.status(404).send('User not found');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
