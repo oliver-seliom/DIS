73864a6ef8ecf5c78fb8e326f4c42076dc062f29
Oliver Hoffman <ojh22@cornell.edu>
2023-10-19 13:46:54 +0200

    samples

--- spring-boot-rest-api/src/main/java/com/example/demo/GreetingController.java
+++ spring-boot-rest-api/src/main/java/com/example/demo/GreetingController.java
@@ -14,6 +14,7 @@ public class GreetingController {
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		Greeting greeting = new Greeting(counter.incrementAndGet(), String.format(template, name));
+    return greeting;
 	}
 }
--- spring-boot-rest-api/src/main/java/com/example/demo/UserController.java
+++ spring-boot-rest-api/src/main/java/com/example/demo/UserController.java
@@ -9,12 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-  // GET /users
+  // GET /users -> returns all users
 	@GetMapping("/users") 
 	public ArrayList<User> users() throws IOException {
     JSONManager dataManager = new JSONManager();
@@ -22,9 +23,9 @@ public class UserController {
 		return users;
 	}
 
-  // GET /users/:id
+  // GET /users/:id -> return a single user
   @GetMapping("/users/{id}") 
-	public ResponseEntity<User> user(@PathVariable int id) throws IOException {
+	public ResponseEntity<User> user(@PathVariable int id, @RequestParam(value = "name", defaultValue = "World") String name) throws IOException {
     JSONManager dataManager = new JSONManager();
     ArrayList<User> users = dataManager.users();
     User foundUser = findUserById(users, id);
@@ -42,11 +43,15 @@ public class UserController {
     JSONManager dataManager = new JSONManager();
     ArrayList<User> users = dataManager.users();
 
-    User user = new User(users.size() + 1, newUser.getName(), newUser.getEmail(), newUser.getRoles(), newUser.isAdmin());
-    users.add(user);
+    newUser.setId(users.size() + 1);
+    users.add(newUser);
+    // User user = new User(users.size() + 1, newUser.getName(), newUser.getEmail(), newUser.getRoles(), newUser.isAdmin());
+    // users.add(user);
+
     dataManager.writeJsonFile("users.json", users);
-    return ResponseEntity.ok(user);
+    return ResponseEntity.ok(newUser);
   }
+
 
   public static User findUserById(List<User> users, int targetId) {
     for (User user : users) {
